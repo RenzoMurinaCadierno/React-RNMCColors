a4830e0be65a63d62b0588ac6757601e7486d344
Renzo Nahuel Murina Cadierno <nmcadierno@hotmail.com>
2020-04-29 20:11:12 -0300

    Added localStorage saving functionality

--- src/App.js
+++ src/App.js
@@ -1,63 +1,77 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom'
-import PaletteList from './containers/PaletteList/PaletteList'
-import Palette from './containers/Palette/Palette'
-import SingleColorPalette from './containers/SingleColorPalette/SingleColorPalette'
-import NewPaletteForm from './containers/NewPaletteForm/NewPaletteForm'
-import initialPalettes from './assets/initialPalettes'
-import { generatePalette } from './assets/colorHelpers'
-import './App.css';
+import React, { Component } from "react"
+import { Route, Switch } from "react-router-dom"
+import PaletteList from "./containers/PaletteList/PaletteList"
+import Palette from "./containers/Palette/Palette"
+import SingleColorPalette from "./containers/SingleColorPalette/SingleColorPalette"
+import NewPaletteForm from "./containers/NewPaletteForm/NewPaletteForm"
+import initialPalettes from "./assets/initialPalettes"
+import { generatePalette } from "./assets/colorHelpers"
+import "./App.css"
 
 class App extends Component {
-
   constructor(props) {
     super(props)
-    this.state = { palettes: initialPalettes }
+    const savedPalettes = JSON.parse(window.localStorage.getItem("RNMCColors"))
+    this.state = { palettes: savedPalettes || initialPalettes }
   }
 
-  findPalette = id =>
-    this.state.palettes.find(palette => palette.id === id)
+  findPalette = (id) => this.state.palettes.find((palette) => palette.id === id)
 
-  savePalette = newPalette =>
-    this.setState({ palettes: [...this.state.palettes, newPalette] })
+  savePalette = (newPalette) => {
+    this.setState({ palettes: [...this.state.palettes, newPalette] }, () =>
+      this.syncLocalStorage()
+    )
+  }
+
+  syncLocalStorage = () =>
+    window.localStorage.setItem(
+      "RNMCColors",
+      JSON.stringify(this.state.palettes)
+    )
 
   render() {
     return (
       <Switch>
         <Route
-          exact path='/palette/new'
-          render={routeProps => 
-            <NewPaletteForm 
-              savePalette={this.savePalette} 
+          exact
+          path="/palette/new"
+          render={(routeProps) => (
+            <NewPaletteForm
+              savePalette={this.savePalette}
               palettes={this.state.palettes}
-              {...routeProps} 
+              {...routeProps}
             />
-          }
+          )}
         />
-        <Route 
-          exact path='/palette/:paletteId/:colorId' 
-          render={routeProps => 
-            <SingleColorPalette 
+        <Route
+          exact
+          path="/palette/:paletteId/:colorId"
+          render={(routeProps) => (
+            <SingleColorPalette
               colorId={routeProps.match.params.colorId}
               palette={generatePalette(
                 this.findPalette(routeProps.match.params.paletteId)
-              )} 
-          />
-          }
+              )}
+            />
+          )}
         />
-        <Route 
-          exact path='/' 
-          render={routeProps => 
+        <Route
+          exact
+          path="/"
+          render={(routeProps) => (
             <PaletteList palettes={this.state.palettes} {...routeProps} />
-          }
+          )}
         />
-        <Route 
-          exact path='/palette/:id' 
-          render={routeProps => 
-            <Palette palette={
-              generatePalette(this.findPalette(routeProps.match.params.id))         
-            } />
-          }
+        <Route
+          exact
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
         />
       </Switch>
     )
