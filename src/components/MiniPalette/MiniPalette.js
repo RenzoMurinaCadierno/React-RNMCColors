9a27ec030754a8468b6db65ac2c15cb82027d1db
Renzo Nahuel Murina Cadierno <nmcadierno@hotmail.com>
2020-04-29 20:37:08 -0300

    Added DeleteIcon to MiniPalette

--- src/components/MiniPalette/MiniPalette.js
+++ src/components/MiniPalette/MiniPalette.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import { withStyles } from '@material-ui/styles'
-import styles from './MiniPalette.styles'
+import React from "react"
+import { withStyles } from "@material-ui/styles"
+import DeleteIcon from "@material-ui/icons/Delete"
+import DeleteOutlinedIcon from "@material-ui/icons/DeleteOutlined"
+import DeleteTwoToneIcon from "@material-ui/icons/DeleteTwoTone"
+import styles from "./MiniPalette.styles"
 
-const MiniPalette = props => {
-
+const MiniPalette = (props) => {
   const { classes, paletteName, emoji, colors, goToPalette } = props
-  const miniColorBoxes = colors.map(color => (
-    <div 
+  const miniColorBoxes = colors.map((color) => (
+    <div
       key={color.name}
-      className={classes.miniColor} 
-      style={{ backgroundColor: color.color}}
+      className={classes.miniColor}
+      style={{ backgroundColor: color.color }}
     />
   ))
 
   return (
     <div className={classes.root} onClick={goToPalette}>
+      <div className={classes.Delete}>
+        <DeleteTwoToneIcon
+          className={classes.DeleteIcon}
+          style={{ transition: "all 0.3s ease-in-out" }}
+        />
+      </div>
       <div className={classes.colors}> {miniColorBoxes} </div>
       <h5 className={classes.title}>
-        {paletteName} 
+        {paletteName}
         <span className={classes.emoji}> {emoji} </span>
       </h5>
     </div>
--- src/components/MiniPalette/MiniPalette.styles.js
+++ src/components/MiniPalette/MiniPalette.styles.js
@@ -1,46 +1,62 @@
 export default {
   root: {
-    backgroundColor: 'rgba(255, 255, 255, 0.6)',
+    backgroundColor: "rgba(255, 255, 255, 0.6)",
     // border: '1px solid black',
-    borderRadius: '5px',
-    padding: '0.5rem',
-    position: 'relative',
-    overflow: 'hidden',
-    boxShadow: '0 0 3px grey, 0 0 5px white',
-    "& :hover": {
-      cursor: 'pointer'
+    borderRadius: "5px",
+    padding: "0.5rem",
+    position: "relative",
+    overflow: "hidden",
+    boxShadow: "0 0 3px grey, 0 0 5px white",
+    cursor: "pointer",
+    "&:hover svg": {
+      opacity: 1
     }
   },
   colors: {
-    backgroundColor: 'transparent',
-    height: '150px',
-    width: '100%',
-    borderRadius: '5px',
-    overflow: 'hidden',
-    boxShadow: '0 0 7px black, 0 0 8px grey',
-    display: 'flex',
-    flexWrap: 'wrap'
+    backgroundColor: "transparent",
+    height: "150px",
+    width: "100%",
+    borderRadius: "5px",
+    overflow: "hidden",
+    boxShadow: "0 0 7px black, 0 0 8px grey",
+    display: "flex",
+    flexWrap: "wrap"
   },
   title: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
     margin: 0,
-    color: 'black',
-    paddingTop: '0.5rem',
-    fontSize: '0.8rem',
-    position: 'relative',
+    color: "black",
+    paddingTop: "0.5rem",
+    fontSize: "0.8rem",
+    position: "relative"
   },
   emoji: {
-    marginLeft: '0.5rem',
-    fontSize: '1.2rem'
+    marginLeft: "0.5rem",
+    fontSize: "1.2rem"
   },
   miniColor: {
-    height: '25%',
-    width: '20%',
+    height: "25%",
+    width: "20%"
     // display: 'inline-block',
     // margin: '0 auto',
     // position: 'relative',
     // marginBottom: '-5px'
+  },
+  Delete: {},
+  DeleteIcon: {
+    color: "red",
+    backgroundColor: "rgba(255, 255, 255, 0.4)",
+    border: "2px solid transparent",
+    borderRadius: "5px",
+    width: "20px",
+    height: "20px",
+    position: "absolute",
+    right: 0,
+    top: 0,
+    padding: "10px",
+    zIndex: 10,
+    opacity: 0
   }
 }
