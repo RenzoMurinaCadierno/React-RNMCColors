15b441b88773c7f6230dca6e95aa7a001d1cc63a
Renzo Nahuel Murina Cadierno <nmcadierno@hotmail.com>
2020-05-06 18:37:07 -0300

    Updates

---
+++ precache-manifest.f3195ce394ef8b3d23a9de6e140eef9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e09c3c325f7951d0813a5d86a3df4a9",
+    "url": "/RenzoMurinaCadierno/React-RNMCColors/index.html"
+  },
+  {
+    "revision": "678d9f5303e223e8a825",
+    "url": "/RenzoMurinaCadierno/React-RNMCColors/static/css/2.69b95c45.chunk.css"
+  },
+  {
+    "revision": "5569885ce2b851c3351e",
+    "url": "/RenzoMurinaCadierno/React-RNMCColors/static/css/main.d70a901d.chunk.css"
+  },
+  {
+    "revision": "678d9f5303e223e8a825",
+    "url": "/RenzoMurinaCadierno/React-RNMCColors/static/js/2.e78a5e8c.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/RenzoMurinaCadierno/React-RNMCColors/static/js/2.e78a5e8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5569885ce2b851c3351e",
+    "url": "/RenzoMurinaCadierno/React-RNMCColors/static/js/main.53de1c7d.chunk.js"
+  },
+  {
+    "revision": "1fb2dceaa7332aec5ce3",
+    "url": "/RenzoMurinaCadierno/React-RNMCColors/static/js/runtime-main.9156fcdb.js"
+  },
+  {
+    "revision": "12a2a44849b9a42027cdff33f8cd54b0",
+    "url": "/RenzoMurinaCadierno/React-RNMCColors/static/media/background.12a2a448.svg"
+  }
+]);
